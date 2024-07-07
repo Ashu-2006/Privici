e9be1b0a80ba2e722328fb79c738f49564c3503c
Ashu-2006 <ashutosh.designs0402@gmail.com>
2024-07-07 18:49:38 +0530

    Ignore files

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cheerio = require('cheerio');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
